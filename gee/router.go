181e20b152fee9a08f020377b1d35bacbfa58b20
xiaolong26 <18710313978@163.com>
2020-06-22 20:16:42 +0800

    trie

--- gee/router.go
+++ gee/router.go
@@ -1,29 +1,79 @@
 package gee
 
 import (
-	"log"
 	"net/http"
+	"strings"
 )
 
 type router struct {
 	handlers map[string]Handlefunc
+	roots	 map[string]*node
 }
 
 func newRouter()*router  {
-	return &router{make(map[string]Handlefunc)}
+	return &router{make(map[string]Handlefunc),make(map[string]*node)}
 }
 
 func (r *router)addRoute(method string,pattern string,handler Handlefunc)  {
-	log.Printf("Route %4s - %s",method,pattern)
+	parts := parsePattern(pattern)
 	key := method + "-" + pattern
+	_,ok := r.roots[method]
+	if !ok{
+		r.roots[method] = &node{}
+	}
+	r.roots[method].insert(pattern,parts,0)
 	r.handlers[key] = handler
 }
 
 func (r *router)handle(c *Context){
-	key := c.Method + "-" + c.Path
-	if handler, ok := r.handlers[key]; ok {
-		handler(c)
+	n, params := r.getRoute(c.Method, c.Path)
+	if n != nil {
+		c.Params = params
+		key := c.Method + "-" + n.pattern
+		r.handlers[key](c)
 	} else {
 		c.String(http.StatusNotFound, "404 NOT FOUND: %s\n", c.Path)
 	}
+}
+
+func parsePattern(pattern string)[]string  {
+	vs := strings.Split(pattern,"/")
+	parts := make([]string,0)
+	for _,item := range vs{
+		if item != ""{
+			parts = append(parts,item)
+			if item[0] == '*'{
+				break
+			}
+		}
+	}
+	return parts
+}
+
+func (r *router) getRoute(method string, path string) (*node, map[string]string) {
+	searchParts := parsePattern(path)
+	params := make(map[string]string)
+	root, ok := r.roots[method]
+
+	if !ok {
+		return nil, nil
+	}
+
+	n := root.search(searchParts, 0)
+
+	if n != nil {
+		parts := parsePattern(n.pattern)
+		for index, part := range parts {
+			if part[0] == ':' {
+				params[part[1:]] = searchParts[index]
+			}
+			if part[0] == '*' && len(part) > 1 {
+				params[part[1:]] = strings.Join(searchParts[index:], "/")
+				break
+			}
+		}
+		return n, params
+	}
+
+	return nil, nil
 }
--- main.go
+++ main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"gee"
+	"net/http"
 )
 
-func main()  {
-	g := gee.New()
-	g.GET("/", func(c *gee.Context) {
-		c.JSON(200,"asd")
+func main() {
+	r := gee.New()
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
-	err := g.Run(":9090")
-	if err!=nil{
-		fmt.Println(err)
-	}
+
+	r.GET("/hello", func(c *gee.Context) {
+		// expect /hello?name=geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
+
+	r.GET("/hello/:name", func(c *gee.Context) {
+		// expect /hello/geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	})
+
+	r.GET("/assets/*filepath", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
+	})
+
+	r.Run(":9999")
 }
 
